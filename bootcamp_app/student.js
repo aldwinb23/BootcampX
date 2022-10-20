fccd91c4c9040ae02b78fb8c0a1ff4a47071aa05
aldwinb <aldwinb23@gmail.com>
2022-10-20 13:13:04 -0700

    parameterized query for student.js and teacher.js

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -8,20 +8,71 @@ const pool = new Pool({
   database: 'aldwinbautista'
 });
 
-pool.query(`
+
+
+// const cohortName = process.argv[2] || 'JUL02';
+// const queryString = 
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = $1
+// ORDER BY teacher;
+// ;
+
+// pool
+// .query(queryString, [cohortName])
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(${row.cohort}: ${row.teacher});
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
+
+
+
+////
+const cohortName = process.argv[2] || 'JUL02';
+const limit = process.argv[3] || '5';
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.name} has an id of ${row.student_id} and was in the ${row.cohort} cohort`);
+    })
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
 
 
+
+
+  
+
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
 
 
 
@@ -68,3 +119,17 @@ LIMIT ${process.argv[3] || 5};
 // })
 // .catch(err => console.error('query error', err.stack));
 
+
+
+
+
+
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'labber',
+//   password: 'labber',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
